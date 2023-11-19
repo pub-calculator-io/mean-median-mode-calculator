2ace87b6bc527cf5d6bbe565ee46fe08b71d206e
Arseniy Olevskiy <arsidian@gmail.com>
2023-11-19 13:27:00 +0400

    calculator changes commit

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Mean, Median, Mode Calculator by Calculator.iO
+Plugin Name: Mean, Median, Mode Calculator by www.calculator.io
 Plugin URI: https://www.calculator.io/mean-median-mode-calculator/
 Description: Calculator for mean, median, and mode in statistics. Use this calculator to get the mean, median, mode, range, and the average for any data set.
 Version: 1.0.0
@@ -16,7 +16,7 @@ if (!function_exists('add_shortcode')) return "No direct call for Mean, Median, 
 
 function display_ci_mean_median_mode_calculator(){
     $page = 'index.html';
-    return '<h2><a href="https://www.calculator.io/mean-median-mode-calculator/" target="_blank"><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48"></a> Mean, Median, Mode Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_mean_median_mode_calculator_iframe"></iframe></div>';
+    return '<h2><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48">Mean, Median, Mode Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_mean_median_mode_calculator_iframe"></iframe></div>';
 }
 
 add_shortcode( 'ci_mean_median_mode_calculator', 'display_ci_mean_median_mode_calculator' );
